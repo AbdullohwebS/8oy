0c5a9f5f45515316f2e6a430cf656dc26876306f
Abdulloh Nurmukhammedov <158317198+AbdullohwebS@users.noreply.github.com>
2025-06-16 11:48:56 +0500

    Update layout.tsx Yangiliklarhga o'zgar tirdim apini notogri qo'yipman

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Cars Json',
-  description: 'Cars Json',
-  generator: 'Cars Json',
+  title: 'Yangiliklar',
+  description: 'Yangiliklar',
+  generator: 'Yangiliklar',
 }
 
 export default function RootLayout({
